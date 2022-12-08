881ec498605d31679debeb7e0d6f716ce00fa945
vitzze <118044884+vitzze@users.noreply.github.com>
2022-12-08 19:50:44 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ for (let i = 0; i < smoothScrollTrigger.length; i++) {
   smoothScrollTrigger[i].addEventListener('click', (e) => {
     e.preventDefault();
     let href = smoothScrollTrigger[i].getAttribute('href');
-    let targetElement = document.getElementById(href.replace('#', ''))
+    let targetElement = document.getElementById(href.replace('#', ''));
     const rect = targetElement.getBoundingClientRect().top;
     const offset = window.pageYOffset;
     const gap = 0; //今回は固定ヘッダなし
